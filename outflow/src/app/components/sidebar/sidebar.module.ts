f29b772a66de043f269895575cc73be29077d02a
VINICIUS KOCK <vinikk03@gmail.com>
2022-10-11 11:38:38 -0300

    sidebar

--- outflow/src/app/components/sidebar/sidebar.module.ts
+++ outflow/src/app/components/sidebar/sidebar.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
 import { SidebarComponent } from "./sidebar.component";
+import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
     declarations: [
-        SidebarComponent
+        // SidebarComponent
     ],
     imports: [
+        MatIconModule,
+        FlexLayoutModule,
+        MatButtonModule
 
     ],
     providers: [],
